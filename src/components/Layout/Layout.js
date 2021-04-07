e19b3d7af138a77fe57ad93f33839c18910918d8
colbyfayock <fayock@gmail.com>
2021-04-06 22:55:56 -0400

    social

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -14,6 +14,10 @@ const Layout = ({ children, displayNav = true, displayShare = true }) => {
 
   const pageTitle = siteName;
 
+  const metaDescription = 'Organize and DRY up submitting your conference proposals!';
+
+  const ogImage = `${homepage}/cfp-template-social.jpg`;
+
   const helmetSettings = {
     defaultTitle: pageTitle,
     titleTemplate: `%s - ${pageTitle}`
@@ -22,11 +26,30 @@ const Layout = ({ children, displayNav = true, displayShare = true }) => {
   return (
     <div className={styles.layoutContainer}>
       <Helmet {...helmetSettings}>
+        <html lang="en" />
+
         <link rel="icon" href="/favicon.ico" />
-        <meta property="og:title" content={pageTitle} />
+        {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" /> */}
+        {/* <link rel="manifest" href="/site.webmanifest" /> */}
+
+        <meta name="description" content={metaDescription} />
+
+        <meta property="og:title" content={siteName} />
+        <meta property="og:description" content={metaDescription} />
         <meta property="og:url" content={homepage} />
         <meta property="og:type" content="website" />
-        <meta property="og:site_name" content={pageTitle} />
+        <meta property="og:site_name" content={siteName} />
+        <meta property="og:image" content={ogImage} />
+        <meta property="og:image:secure_url" content={ogImage} />
+        <meta property="og:image:width" content="2024" />
+        <meta property="og:image:height" content="1012" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:image" content={ogImage} />
+        <meta property="twitter:site" content={homepage} />
+        <meta property="twitter:creator" content="@colbyfayock" />
+
       </Helmet>
 
       { displayNav && <Nav />}
